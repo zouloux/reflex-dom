ceb98c868d649fa0ac79d76ea8b15d3b74dd433b
Alexis Bouhet <alexis@les-hallucines.studio>
2022-07-02 19:30:32 +0200

    Patched demos

--- parcel-demo.sh
+++ parcel-demo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Copy no-bundle demo
-mkdir -p "./public/5-no-bundler-demo/"
-cp "./demos/5-no-bundler-demo/index.html" "./public/5-no-bundler-demo/index.html"
+mkdir -p "./public/demos/5-no-bundler-demo/"
+cp "./demos/demos/5-no-bundler-demo/index.html" "./public/demos/5-no-bundler-demo/index.html"
 
-cp "./demos/index.html" "./public/index.html"
+cp "./demos/index.html" "./public/demos/index.html"
 
 if [ "$1" = "build" ]
 then
