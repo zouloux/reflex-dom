c91dd3cc36a4ce15a2e5c2b58afac3470a445ae4
Alexis Bouhet <alexis@les-hallucines.studio>
2023-02-08 17:44:27 +0100

    Added sub scripts

---
+++ dev/example.tsx
@@ -0,0 +1,38 @@
+import { h, render, state, changed, ref, mounted } from "@zouloux/reflex";
+
+// Reflex components can be pure functions or factory functions
+function ReflexApp(props) {
+	// How basic state works
+	const counter = state(0);
+	const increment = () => counter.value++;
+	const reset = () => (counter.value = 0);
+	/*
+	 // No need to use ref for locally scoped variables
+	 let firstUpdate = true;
+	 // Detect changes of states or props
+	 changed(
+	 console.log(`Counter just updated to ${newValue}`, firstUpdate);
+	 firstUpdate = false;
+	 });
+	 */
+	// How refs of dom elements works
+	const title = ref<HTMLDivElement>();
+	mounted(() => console.log(title.dom.innerHTML));
+
+	// Returns a render function
+	// Classes can be arrays ! Falsy elements of the array will be discarded
+	return () => (
+		<div class={["ReflexApp", props.modifier, false]}>
+			<h1 ref={title}>Hello from Reflex {props.emoji}</h1>
+			<button onClick={increment}>Increment</button>&nbsp;
+			<button onClick={reset}>Reset</button>&nbsp;
+			<span>Counter : {counter}</span>
+		</div>
+	);
+}
+
+// Render it like any other v-dom library
+render(
+	<ReflexApp modifier="ReflexApp-lightMode" emoji="👋" />,
+	document.getElementById("app")
+);
