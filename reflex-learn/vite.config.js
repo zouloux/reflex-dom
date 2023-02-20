bdab9edf485c46b1395d270202731a444fd8c3b4
Alexis Bouhet <alexis@les-hallucines.studio>
2023-02-20 21:10:56 +0100

    Learn

--- reflex-learn/vite.config.js
+++ reflex-learn/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import { reflexRefresh } from "reflex-dom/hmr-plugin";
 import { resolve } from "path";
 
-export default defineConfig( () => {
+export default defineConfig( c => {
+	console.log();
 	return {
 		root: resolve("./src"),
+		base: c.mode === "production" ? '/reflex/learn' : '',
 		build: {
 			outDir: resolve("../docs/learn/"),
 			manifest: true,
